ea9e73279e7e7493d5f8d250f233441585f328ca
Yahya Ewida <yahyaewida@yahoo.com>
2020-04-18 10:43:14 -0700

    entity structure

---
+++ SportyApp/model/Entities/Sport.swift
@@ -0,0 +1,22 @@
+//
+//  Sport.swift
+//  ProjectDemo
+//
+//  Created by Yahya Ewida on 4/18/20.
+//  Copyright © 2020 Yahya Ewida. All rights reserved.
+//
+
+import Foundation
+
+
+struct Sport {
+    
+    var idSport : String
+    var strSport: String
+    var strFormat : String
+    var strSportThumb : String
+    var strSportThumbGreen : String
+    var strSportDescription : String
+    
+    
+}
